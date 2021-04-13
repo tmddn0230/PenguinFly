e772e6ab5b37ae72551cfd3d897d81c25bcd01bc
YU <82423176+tmddn0230@users.noreply.github.com>
2021-04-13 14:49:01 +0900

    3rd

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Enermy : MonoBehaviour
 {
     public float speed = 5;
     Vector3 dir;
@@ -15,14 +15,8 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dir = Vector3.down;
+        dir = Vector3.back;
         dir.Normalize();
         transform.position = transform.position + dir * speed * Time.deltaTime;
-    }
-    private void OnCollisionEnter(Collision collision)     // 충돌할 때 실행됨  Collision을 담는 Collision 변수 
-    {
-        
-        Destroy(gameObject);   
-
     }
 }
