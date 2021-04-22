a8dcb9cb36f38ef0a0176e6e005d61c4253c9423
YU <82423176+tmddn0230@users.noreply.github.com>
2021-04-22 17:31:05 +0900

    1

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public float speed = 5;
-   
-    
-   
+
+
+
     Vector3 dir;
     // Start is called before the first frame update
     private void Awake()
     {
-        
-       
+
+
     }
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
@@ -28,18 +28,32 @@ public class Item : MonoBehaviour
         transform.position = transform.position + dir * speed * Time.deltaTime;
         Destroy(gameObject, 10);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            gameObject.SetActive(false);
+            GameObject smobj = GameObject.Find("ScoreManager");
+            ScoreManager sm = smobj.GetComponent<ScoreManager>();
+            sm.AddScore(100);
+            print("고기 먹음!!!");
+
+        }
+    }
     private void OnCollisionEnter(Collision collision)     // 충돌할 때 실행됨  Collision을 담는 Collision 변수 
     {
-       if(collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 6)
         {
-        gameObject.SetActive(false);
-        GameObject smobj = GameObject.Find("ScoreManager");
-        ScoreManager sm = smobj.GetComponent<ScoreManager>();
-        sm.AddScore(100);
+            gameObject.SetActive(false);
+            GameObject smobj = GameObject.Find("ScoreManager");
+            ScoreManager sm = smobj.GetComponent<ScoreManager>();
+            sm.AddScore(100);
+            print("고기 먹음!!!");
 
         }
 
-        
+
 
 
         // Destroy(gameObject);
